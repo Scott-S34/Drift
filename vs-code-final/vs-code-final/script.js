78726a793f5a4e8ea2c0096fa67fa215944c8219
Scott-S34 <=>
2025-04-09 20:41:47 +0100

    security and file uploading

--- vs-code-final/vs-code-final/script.js
+++ vs-code-final/vs-code-final/script.js
@@ -32,14 +32,51 @@ document.getElementById("submitButton").addEventListener("click", function() {
     window.location.href = "thankyou.html";
 });
 
-//securing the subscribe button against XSS
-function secureForm(form){
+//securing the subscribe button against Cross-Site Scripting XSS
+function secureForm(){
+  //get the form with the email subscription
   var form = document.getElementById("subscribeForm");
+  //get the email input box
   var email = document.getElementById("email");
+  //replace some special characters with their URL encoding 
   var encoding = String(email.value).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  //set the value of the email to the URL encoding version of the user input
   email.value = encoding;
   //alert("email secured")
+  //return the email to check that XSS has been mitigated
   form.innerHTML= "Email secured: " + email.value;
-  form.submit();
+  //submit the subscribe form
+}
 
+//providing a security mechanism against file upload vulnerabilities
+//these help to prevent big files or malicious code files from being executed and destroying the website
+function secureFileUploading(){
+  var scanFunction = document.getElementById("scanner");
+  //get the file uploaded by the user
+  var fileUpload = document.getElementById('upload');
+  //acquire the file size
+  var fileSize = fileUpload.files[0].size;
+  //set a limit for the maximum file size
+  var maxSize = 12  * 1024; //12KB;
+  //check the size of the file
+  alert(fileSize + "B" + ", " + (fileSize /1024) + "MB");
+  //check that the filesize is in the limit
+  if(fileSize > maxSize){
+    alert("Sorry! File size is too big!");
+    return;
+  }
+
+  //acquire the supplied filename and its extension
+  var fileName = fileUpload.value;
+  var fileParts = fileName.split(".");
+  var extension = String(fileParts.pop().toLowerCase());
+  alert("Extension = " + extension);  
+  //if the file type is jpg, jpeg, or png, proceed 
+  if( fileSize < maxSize && ( (extension === "jpg") || (extension === "png") || (extension == "jpeg") ) ){
+    alert("Thank you! Result is loading");
+    scanFunction.submit();
+  } else{   
+    alert("Insufficient file extension, image should be either png, jpg or jpeg");
+    return;
+  }
 }
